9693c4e87848337ead20d9c0bb6d042e21ab3689
Loïc <loic.decoster@isen.yncrea.fr>
2018-02-09 15:50:26 +0100

    Créations premiers fichiers Phaser

---
+++ Jeu/js/main.js
@@ -0,0 +1,13 @@
+var game = new Phaser.Game(1350, 750, Phaser.AUTO, '', { preload:preload, create:create, upload:upload });
+
+var preload = function() {
+
+};
+
+var create = function() {
+
+};
+
+var upload = function() {
+
+};
