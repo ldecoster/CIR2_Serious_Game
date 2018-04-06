9ea9159c13d161d61deaddca806ecbac43cc588c
Unknown <marine.clarisse@isen.yncrea.fr>
2018-04-06 14:04:43 +0200

    Ajout Menu de démarrage
    Ajout d'une image de fond et d'un bouton Jouer
    Ajout du code dans le State Menu.js

--- Jeu/js/states/menu.js
+++ Jeu/js/states/menu.js
@@ -6,17 +6,24 @@ define(['js/models/System.js', 'text!assets/json/config.json'],
 
 		menu.prototype = {
 			preload: function() {
-
+				this.game.load.image('background','assets/img/menu_background.png');
+				this.game.load.spritesheet('button', 'assets/img/button_play.png', 725, 325);
 			},
 
 			create: function() {
 				this.system = new System(this.game);
 				this.system.createFullScreen();
-				this.game.state.start('Play', true, false, configFromFile);
+
+				var actionOnClick = function() {
+					this.game.state.start('Play', true, false, configFromFile);
+				};
+
+			    var background = this.game.add.tileSprite(0, 0, 1350, 750, 'background');
+			    var button = this.game.add.button(this.game.world.centerX - 95, 400, 'button', actionOnClick, this, 2, 1, 0);
 			},
 
 			update: function() {
-				
+
 			}
 		};
 
