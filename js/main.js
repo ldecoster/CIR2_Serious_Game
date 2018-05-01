e02048203546fb4d37114f45539a22463f8e63e9
Loïc <loic.decoster@isen.yncrea.fr>
2018-05-01 18:24:55 +0200

    Intégration du contrôleur principal dans main.js
    Déplacement du code de controllers/game.js vers main.js

--- js/main.js
+++ js/main.js
@@ -18,7 +18,22 @@ require.config({
 	}
 });
 
-require(['js/controllers/game'],
-	function (Game) {
-		var game = new Game('game');
+require(['phaser', 'js/states/boot', 'js/states/load', 'js/states/menu', 'js/states/play', 'js/states/skills', 'js/states/win', 'js/states/defeat'],
+	function (Phaser, boot, load, menu, play, skills, win, defeat) {
+		var jeu = function (htmlFrame) {
+			var game = new Phaser.Game(1350, 750, Phaser.AUTO, htmlFrame, {});
+
+			game.state.add('Boot', boot);
+			game.state.add('Load', load);
+			game.state.add('Menu', menu);
+			game.state.add('Play', play);
+			game.state.add('Skills', skills);
+			game.state.add('Win', win);
+			game.state.add('Defeat', defeat);
+
+			game.state.start('Boot');
+
+		};
+		
+		jeu('game');
 	});
