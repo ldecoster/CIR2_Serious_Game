aba7f36fbedf8ddcfba979cabebf7adbc2d34813
Unknown <matthieularosasimon@gmail.com>
2018-05-13 17:37:44 +0200

    Couleur mission

--- js/states/missions.js
+++ js/states/missions.js
@@ -19,10 +19,13 @@ define(['phaser', 'js/models/System.js'],
 				this.policier = this.game.add.sprite(100, 428, 'policier');
 				this.bulle = this.game.add.sprite(200, 10, 'bulle');
 
-				this.missionDisplay = this.game.add.text(270, 100, '° Débloquer toutes les compétences rouges', {fontSize: '25px', fill: '#000000'})+
-									  this.game.add.text(270, 130, '° Débloquer toutes les compétences vertes', {fontSize: '25px', fill: '#000000'})+
-									  this.game.add.text(270, 160, '° Débloquer toutes les compétences orange', {fontSize: '25px', fill: '#000000'})+
-									  this.game.add.text(270, 190, '° Débloquer toutes les compétences', {fontSize: '25px', fill: '#000000'});
+				this.missionDisplay = this.game.add.text(270, 100, '- Débloquer toutes les compétences', {fontSize: '25px', fill: '#000000'})+
+									  this.game.add.text(705, 100, 'rouges', {fontSize: '25px', fill: '#ff0000'})+
+									  this.game.add.text(270, 130, '- Débloquer toutes les compétences', {fontSize: '25px', fill: '#000000'})+
+									  this.game.add.text(705, 130, 'vertes', {fontSize: '25px', fill: '#00ff00'})+
+									  this.game.add.text(270, 160, '- Débloquer toutes les compétences', {fontSize: '25px', fill: '#000000'})+
+									  this.game.add.text(705, 160, 'orange', {fontSize: '25px', fill: '#ffa500'})+
+									  this.game.add.text(270, 190, '- Débloquer toutes les compétences', {fontSize: '25px', fill: '#000000'});
 
 				this.system = new System(this.game);
 				this.system.createFullScreen();
