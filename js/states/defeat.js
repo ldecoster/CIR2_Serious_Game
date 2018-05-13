8067ca9a6e40e22d8471b938d07e1e953ada351a
Unknown <matthieularosasimon@gmail.com>
2018-05-13 19:01:48 +0200

    Texte fin defeat et win

--- js/states/defeat.js
+++ js/states/defeat.js
@@ -20,6 +20,8 @@ define(['js/models/System.js', 'text!assets/json/config.json', 'text!assets/json
 				var background = this.game.add.tileSprite(0, 0, 1350, 750, 'backgroundDefeat');
 				var button_menu = this.game.add.button(300, 620, 'buttonMenu', menuClick, this, 0, 1);
 				var button_replay = this.game.add.button(830, 620, 'buttonReplay', replayClick, this, 0, 1);
+
+				this.defeatDisplay = this.game.add.text(400, 400, 'La France n\'a pas pu être sauvée de la pollution...', {fontSize: '25px', fill: '#f65050', stroke: '#000000', strokeThickness: 4});
 			}
 		};
 
--- js/states/win.js
+++ js/states/win.js
@@ -6,6 +6,7 @@ define(['js/models/System.js', 'text!assets/json/config.json', 'text!assets/json
 
 		win.prototype = {
 			init: function(pollutionRate) {
+				this.pollutionRate = pollutionRate;
 				console.log(pollutionRate);
 			},
 
@@ -24,6 +25,10 @@ define(['js/models/System.js', 'text!assets/json/config.json', 'text!assets/json
 			    var background = this.game.add.tileSprite(0, 0, 1350, 750, 'backgroundWin');
 			    var button_menu = this.game.add.button(300, 620, 'buttonMenu', menuClick, this, 0, 1);
 			    var button_replay = this.game.add.button(830, 620, 'buttonReplay', replayClick, this, 0, 1);
+
+			    this.winDisplay = this.game.add.text(180, 230, 'Félicitations ! Grâce à vous la France a atteint       % de pollution.', {fontSize: '25px', fill: '#f3cd35', stroke: '#000000', strokeThickness: 2})+
+			    				  this.game.add.text(740, 230, this.pollutionRate, {fontSize: '25px', fill: '#ffffff', stroke: '#000000', strokeThickness: 2})+
+			    				  this.game.add.text(180, 260, 'Ce niveau raisonnable ne la met plus en danger !', {fontSize: '25px', fill: '#f3cd35', stroke: '#000000', strokeThickness: 2});
 			}
 		};
 
